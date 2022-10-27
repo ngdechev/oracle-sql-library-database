c810ddd997ff6f21bd1829f4ff354f3a26e8c419
N. Dechev <71671021+ngdechev@users.noreply.github.com>
2022-10-27 21:55:20 +0300

    Added insert.sql
    Added insert.sql file which contains insertcommands.

---
+++ insert.sql
@@ -0,0 +1,96 @@
+-- Добавям информация в таблицата „Kniga“
+
+INSERT INTO Kniga VALUES('PodIgoto',1893,'Roman',100,"Pod Igoto",1,1000,'Y');
+ 
+INSERT INTO Kniga VALUES('Pesenta na koleletata',1933 ,'Hudojestveno Proizvedenie',101,"Pesenta na koleletata",2,1001,'N');
+ 
+INSERT INTO Kniga VALUES('kosachi',1905,'Razkaz',102,"Kosachi",3,1002,'Y');
+ 
+INSERT INTO Kniga VALUES('Zatochenici',1901,'Stihotvorenie',103,"Zatochenici",4,1003,'Y');
+ 
+INSERT INTO Kniga VALUES('Snqg',1967,'Piesa',104,"Snqg",5,1004,'N');
+ 
+INSERT INTO Kniga VALUES('Kradecat na praskovi',1948,'Povest',105,"Kradecat na praskovi",6,1005,'Y');
+ 
+INSERT INTO Kniga VALUES('Maice si',1867,'Stihotvorenie',106,"Maice si",7,1006,'N');
+ 
+-- Добавям информация в таблицата „Avtor“
+
+INSERT INTO Avtor VALUES(1000,"Ivan Vazov");
+ 
+INSERT INTO Avtor VALUES(1001,"Iordan Iovkov");
+ 
+INSERT INTO Avtor VALUES(1002,"Elin Pelin");
+ 
+INSERT INTO Avtor VALUES(1003,"Peio Iavorov");
+ 
+INSERT INTO Avtor VALUES(1004,"Valeri Petrov");
+ 
+INSERT INTO Avtor VALUES(1005,"Emiliqn Stanev");
+ 
+INSERT INTO Avtor VALUES(1006,"Hristo Botev");
+ 
+-- Добавям информация в таблицата „Chitatel“
+
+INSERT INTO Chitatel VALUES("Stefan","0878112233",1);
+ 
+INSERT INTO Chitatel VALUES("Magdalena","0878432167",2);
+ 
+INSERT INTO Chitatel VALUES("Ivan","0878212698",3);
+ 
+INSERT INTO Chitatel VALUES("Petq","0878072034",4);
+ 
+INSERT INTO Chitatel VALUES("Mariq","0878549076",5);
+ 
+INSERT INTO Chitatel VALUES("Ivo","0878963056",6);
+ 
+INSERT INTO Chitatel VALUES("Georgi","0878101010",7);
+ 
+-- Добавям информация в таблицата „Izdatelstvo“
+
+INSERT INTO Izdatelstvo VALUES(101);
+ 
+INSERT INTO Izdatelstvo VALUES(102);
+ 
+INSERT INTO Izdatelstvo VALUES(103);
+ 
+INSERT INTO Izdatelstvo VALUES(104);
+ 
+INSERT INTO Izdatelstvo VALUES(105);
+ 
+INSERT INTO Izdatelstvo VALUES(106);
+ 
+INSERT INTO Izdatelstvo VALUES(107);
+ 
+-- Добавям информация в таблицата „Slujitel“
+
+INSERT INTO Slujitel VALUES("Stefka","Menidjyr","0878908721",101);
+ 
+INSERT INTO Slujitel VALUES("Hristo","Bibliotekar","0878456798",102);
+ 
+INSERT INTO Slujitel VALUES("Ivo","Bibliotekar","0878098721",103);
+ 
+INSERT INTO Slujitel VALUES("Marin","Bibliotekar","0878300218",104);
+ 
+INSERT INTO Slujitel VALUES("Radina","Konsultant","0878734072",105);
+ 
+INSERT INTO Slujitel VALUES("Tanja","Poddryjka","0878092267",106);
+ 
+INSERT INTO Slujitel VALUES("Nikol","Poddryjka","0878986513",107);
+ 
+-- Добавям информация в таблицата „Zaemane“
+
+INSERT INTO Zaemane VALUES('2019-02-10',1,'2019-03-10',101,1);
+ 
+INSERT INTO Zaemane VALUES('2019-02-13',2,'2019-03-23',102,2);
+ 
+INSERT INTO Zaemane VALUES('2019-02-13',3,'2019-02-23',103,3);
+ 
+INSERT INTO Zaemane VALUES('2019-02-16',4,'2019-02-29',104,4);
+ 
+INSERT INTO Zaemane VALUES('2019-02-16',5,'2019-02-22',105,5);
+ 
+INSERT INTO Zaemane VALUES('2019-02-20',6,'2019-03-05',106,6);
+ 
+INSERT INTO Zaemane VALUES('2019-03-01',7,'2019-04-01',107,7);
+ 
