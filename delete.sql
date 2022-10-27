4fed0bcaca3ea9e49fbcd90937d816e1971b2474
N. Dechev <71671021+ngdechev@users.noreply.github.com>
2022-10-27 21:56:51 +0300

    Added delete.sql
    Added delete.sql file which contains delete commands.

---
+++ delete.sql
@@ -0,0 +1,23 @@
+-- Изтривам записи от таблицата „Kniga“
+ 
+DELETE FROM Kniga WHERE kniga_id = 4;
+ 
+-- Изтривам записи от таблицата „Avtor“
+ 
+DELETE FROM Avtor;
+ 
+-- Изтривам записи от таблицата „Chitatel“
+ 
+DELETE FROM Chitatel WHERE Telefon = "0878212698";
+ 
+-- Изтривам записи от таблицата „Izdatelstvo“
+ 
+DELETE FROM Izdatelstvo;
+ 
+-- Изтривам записи от таблицата „Slujitel“
+ 
+DELETE FROM Slujitel WHERE Ime = "Marin";
+ 
+-- Изтривам записи от таблицата „Zaemane“
+ 
+DELETE FROM Zaemane WHERE Zaemane_id = 5;
